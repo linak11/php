364e8c97680dcf918a3620b5f20e5cacf294968e
zaharat <zaharat_b@hotmail.fr>
2021-01-14 13:54:19 +0100

    mise en place du fichier index

---
+++ exo1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exercice 1</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="">
+    
+    </head>
+    
+    <body>
+       
+        <?php
+            $name="toto";
+            echo "$name"; 
+        ?>
+  
+    </body>
+</html>
